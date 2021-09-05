8e96c070b29d2aef8298ffafd62b11565f6456da
Yangwenli-hub <olivier-pepin@outlook.com>
2021-09-05 15:33:11 +0200

    Small changes (addind code / UE)

--- Exercices/Exercice_1.py
+++ Exercices/Exercice_1.py
@@ -2,40 +2,68 @@
 
 # Conditionals exercises
 
-## Exercise 1.1 Get user input using input(“Enter your age: ”).
+# Exercise 1.1 Get user input using input(“Enter your age: ”).
 ## If user is 18 or older, give feedback: You are old enough to drive. 
 ## If below 18 give feedback to wait for the missing amount of years.
 
-saisie = input('Entrer votre age : ') 
-saisie = int(saisie)
-if saisie >= 18:
+saisie = input('Entrer votre age : ') # Saisie d'une variable en String
+
+saisie_int = int(saisie) # Transformation variable saisie string en une variable integer
+if saisie_int >= 18:
     print('Tu es assez agé pour apprendre a conduire.')
 else:
-    diff = 18 - saisie 
-    print('Tu dois attendre ' + str(diff) + ' annee(s) pour apprendre a conduire.')
+    diff = 18 - saisie_int 
+    # Retranformation variable integer en string pour concatenation
+    print('Tu dois attendre ' + str(diff) + ' annee(s) pour apprendre a conduire.') 
 
 
-## Exercise 1.2 Compare the values of my_age and your_age using if … else. 
+# Exercise 1.2 Compare the values of my_age and your_age using if … else.
 ## Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. 
 ## You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, 
 ## and a custom text if my_age = your_age.
 
-son_age = int(input('Entrer son age : '))
-votre_age = int(input('Entrer votre age : '))
-diff = votre_age - son_age
-if son_age < votre_age:
-    print('Vous etes ' + str(abs(diff)) + ' ans plus vieux que lui.')
+
+# With if / else (years difference not supported)
+
+your_age = int(input('Entrer your age : ')) # Transformation directe des varibles string en integer
+my_age = int(input('Entrer my age : '))
+
+diff = my_age - your_age
+
+if your_age < my_age:
+    print('Vous etes ' + str(abs(diff)) + ' ans plus jeune que moi.')
 else: 
-    if son_age > votre_age:
-        print('Vous etes ' + str(abs(diff)) + ' ans plus jeune que moi.')
+    if your_age > my_age:
+        print('Vous etes ' + str(abs(diff)) + ' ans plus vieux que moi.')
     else:
         print('Nous avons le meme age.')
 
-## Exercice 1.3 Get two numbers from the user using input prompt. If a is greater than b return a is greater than b,
+
+# With elif (years implemented)
+
+my_age = int(input("Enter my age : "))
+your_age = int(input("Enter your age : "))
+
+diff_age = my_age - your_age
+
+year_label = "year"
+if abs(diff_age) > 1:
+    year_label = year_label + "s"
+
+if diff_age > 0:
+    print("You're",diff_age, year_label, "younger than me")
+elif diff_age == 0:
+    print("We have the same age")
+else:
+    print("You're",abs(diff_age), year_label, "older than me")
+
+
+# Exercice 1.3 Get two numbers from the user using input prompt. If a is greater than b return a is greater than b,
 ## if a is less b return a is smaller than b, else a is equal to b.
 
 a = int(input('Entrer le nombre a : '))
 b = int(input('Entrer le nombre b : '))
+
 if a > b:
     print(str(a), 'est plus grand que', str(b))
 elif a < b:
