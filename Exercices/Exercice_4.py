7d0f143b19b9f2f35938105657f1e604a9360c21
Yangwenli-hub <olivier-pepin@outlook.com>
2021-09-06 09:29:43 +0200

    MAJ_06/09/21

--- Exercices/Exercice_4.py
+++ Exercices/Exercice_4.py
@@ -6,9 +6,21 @@ import numpy
 
 # Exercice 1
 
+# 1 Declare an empty list
+
 list = []
+
+# 2 Declare a list with more than 5 items
+
 list = [1,2,3,4,5,6]
+
+# 3 Find the length of your list
+
+list = [1,2,3,4,5,6]
+
+
 print(len(list)/2)
+
 print(list[0],list[2],list[-1])
 
 mixed_data_types = ['name','age','height','marital status','adress']
