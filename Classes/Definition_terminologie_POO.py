17aaecae5d4b44cc66190e7cfddbcc519d7a1b06
Yangwenli-hub <olivier-pepin@outlook.com>
2021-09-07 20:49:24 +0200

    Push File Def OOP

---
+++ Classes/Definition_terminologie_POO.py
@@ -0,0 +1,18 @@
+## Definition / Terminologie PPO (Programmation Orienté-Objet) pour Python
+
+# Classe    : Plan de conception, genre (ex: Humain)
+# Objet     : instance de classe (ex : Julien). Instancier = créer un objet.
+
+# Attribut  : variable de classe (ex : prenom, age, sexe, taille ...)
+# Propriété : manière de manipuler les attributs (lecture seule, accès non autorisé en dehors de la classe, etc.)
+
+# Methode           : fonction d'une classe (ex : manger, marcher, parler, dormir, mourir)
+# Methode de classe : fonction d'une classe
+# Methode statique  : fonction d'une classe, mais indépendante de celle-ci.
+
+# Héritage          : classe Fille qui hérite d'une classe Mère (Fille est une sorte de Mère)
+# Exemple Héritage  : classe Chat hérite de la classe Animal (Chat est une sorte d'Animal)
+
+# Avantages POO : - meilleur organisation code
+#               : - réutilisation possible
+#               : - beaucoup plus facilement maintenable en travail collaboratif
